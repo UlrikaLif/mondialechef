ac489a32b4e2a455caa7a1fe4a406ddbb4c6ae4d
Ulrika L <u.lifvenborg@outlook.com>
2021-09-08 12:05:50 +0200

    Code for useState - chapter 10

--- src/view/SigninView.js
+++ src/view/SigninView.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const SigninView = () => {
+    const [loggedInUser, setLoggedInUser] = useState("Arasto")
 
     return(
         <div>
-            <h1>This is the SignView</h1>
+            <h1>{loggedInUser}</h1>
+            <button onClick={() =>setLoggedInUser("Hulken")}>Update the state value</button>
         </div>
 
     )
