a9bb8f2798ff59548c8dbc266474a990fc023b53
Ulrika L <u.lifvenborg@outlook.com>
2021-09-08 14:35:59 +0200

    Using global values

--- src/components/navigationbar/NavigationBar.js
+++ src/components/navigationbar/NavigationBar.js
@@ -1,20 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './NavigationBar.css'
 import logotype from "../../shared/images/logotype.svg"
 import { useHistory } from 'react-router-dom'
-
+import { UserContext } from "../../shared/global/provider/UserProvider"
 
 export const NavigationBar = () => {
     const history = useHistory()
+    const [authenticatedUser, setAuthenicatedUser] = useContext(UserContext)
 
     return(
         <div className="navigationBarWrapper">
             <img onClick={() => history.push('/')}
-            className="logotype"
-            src={logotype} 
-            alt="Error..." />
+                className="logotype"
+                src={logotype} 
+                alt="Error..." />
             <span onClick={() => history.push('/signin')}
-            className="signin">Sign in</span>
+                className="signin">Sign in</span>
+            <h3> {authenticatedUser}</h3>
         </div>
     )
 }
