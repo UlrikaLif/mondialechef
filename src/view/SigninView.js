efa8c36abd8a652712a200b978fc39a957d28a90
Ulrika L <u.lifvenborg@outlook.com>
2021-09-08 13:24:34 +0200

    useState with username and password

--- src/view/SigninView.js
+++ src/view/SigninView.js
@@ -1,12 +1,12 @@
 import React, {useState} from 'react'
 
 export const SigninView = () => {
-    const [loggedInUser, setLoggedInUser] = useState()
+    const [username, setUsername] = useState()
     const [password, setPassword] = useState()
 
     return(
         <div>
-            <span> Username:  </span><input onChange={event =>setLoggedInUser(event.target.value)} /> <br />
+            <span> Username:  </span><input onChange={event =>setUsername(event.target.value)} /> <br />
             <span> Password:  </span><input type="password" onChange={event =>setPassword(event.target.value)} /> <br />
             <button>Login</button>
         </div>
